3f93c8820d42de5871c84904e1341a220deb6e22
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-02-23 04:49:32 -0300

    Initial commit

---
+++ src/test/java/br/com/vitorgabrielti/eventsmicroservice/EventsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitorgabrielti.eventsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
