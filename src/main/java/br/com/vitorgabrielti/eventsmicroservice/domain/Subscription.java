606c1f1e4679f3e4f2a1cfdda303c94eef3d3bf2
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-02-24 02:33:37 -0300

    Criando o domínio

---
+++ src/main/java/br/com/vitorgabrielti/eventsmicroservice/domain/Subscription.java
@@ -0,0 +1,24 @@
+package br.com.vitorgabrielti.eventsmicroservice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name="subscription")
+@Table(name="subscription")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Event event;
+
+    private String participantEmail;
+
+}
